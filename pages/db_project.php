a7c592c8bd38a62905a785be4226a438d293d6f3
hyy369 <HYY@HYYs-MacBook-Pro.local>
2017-04-04 23:27:18 -0400

    add web interfaces

---
+++ pages/db_project.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>CS421 Database G11 Project "Class Booking Service"</title>
+  <meta name="Author" content="Yangyang He">
+</head>
+<body>
+  <h1>CS421 Database G11 "Class Booking Service" Project</h1>
+  <ul>
+    <li>Shennie Yao</li>
+    <li>Nicholas Whays (Contact Person)</li>
+    <li>Yangyang He</li>
+  </ul>
+  <h1>We will post our project here!</h1>
+  <ul>
+    <li><a href="assets/Stage_1.pdf">Stage 1 (PDF)</a></li>
+    <li><a href="assets/SACT_STAGE_2.pdf">Stage 2 (PDF)</a></li>
+  </ul>
+  <h1>CBS - Classroom Booking System</h1>
+  <form action="search_room.php" method="post">
+    <select  name="room">
+      <?php
+      // Performing SQL query
+      $dbconn = pg_connect("host=db.cs.wm.edu dbname=swyao_CBS user=nswhay password=nswhay")
+      or die('Could not connect:' . pg_last_error());
+      $query = "SELECT room_id FROM rooms";
+      $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+      while ($line = pg_fetch_array($result, null, PGSQL_NUM)) {
+        echo '<option value="';
+        echo $line[0];
+        echo '">';
+        echo $line[0];
+        echo '</option>\n';
+      }
+      ?>
+    </select>
+    <INPUT TYPE="submit" name="submit" />
+  </form>
+</body>
+</html>
---
+++ pages/search_room.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+  <title>CS421 Database G11 Project "Class Booking Service"</title>
+  <meta name="Author" content="Yangyang He">
+</head>
+<body>
+  <table>
+  <tr>
+    <td>Building</td>
+    <td>Room No.</td>
+    <td>Projector</td>
+    <td>Board</td>
+    <td>Visualizer</td>
+    <td>Outlets No.</td>
+    <td>Capacity</td>
+  </tr>
+  <?php
+  // Performing SQL query
+  $dbconn = pg_connect("host=db.cs.wm.edu dbname=swyao_CBS user=nswhay password=nswhay")
+  or die('Could not connect:' . pg_last_error());
+  $query = "SELECT * FROM rooms WHERE room_id='" . $_POST["room"]. "'";
+  $result = pg_query($query) or die('Query failed: ' . pg_last_error());
+
+  while ($line = pg_fetch_array($result, null, PGSQL_NUM)) {
+    echo "\t<tr>\n";
+    echo "\t\t<td>$line[1]</td>\n";
+    echo "\t\t<td>$line[2]</td>\n";
+    echo "\t\t<td>$line[3]</td>\n";
+    echo "\t\t<td>$line[4]</td>\n";
+    echo "\t\t<td>$line[5]</td>\n";
+    echo "\t\t<td>$line[6]</td>\n";
+    echo "\t\t<td>$line[7]</td>\n";
+    echo "\t</tr>\n";
+  }
+  ?>
+</table>
+</body>
+</html>
+<? php
+// Free resultset
+pg_free_result($result);
+
+// Closing connection
+pg_close<$dbconn);
+?>
