626a336df1cbcb49de09930f84f80ff1f73eaee3
hyy369 <hyy369@users.noreply.github.com>
2017-04-16 15:23:35 -0400

    test

--- view.php
+++ view.php
@@ -53,7 +53,9 @@
             //$dbconn = pg_connect("host=db.cs.wm.edu dbname=swyao_CBS user=nswhay password=nswhay")
             //  or die('Could not connect:' . pg_last_error());
             //$sql = "SELECT * FROM times WHERE room_id=$_GET['room'];";
+            echo "\t<tr>\n";
             echo $_GET['room'];
+            echo "\t</tr>\n";
             //$result = pg_query($sql) or die('Query failed: ' . pg_last_error());
 
             // while ($line = pg_fetch_array($result, null, PGSQL_NUM)) {
