db07f822d978e7e6c4a5251cdbf181999bf05ef3
hyy369 <hyy369@users.noreply.github.com>
2017-04-16 15:12:35 -0400

    test

--- view.php
+++ view.php
@@ -47,11 +47,11 @@
         <table id="roomTable">
           <?php
             // Connecting, selecting database
-            $dbconn = pg_connect("host=db.cs.wm.edu dbname=swyao_CBS user=nswhay password=nswhay")
-              or die('Could not connect:' . pg_last_error());
-            $sql = "SELECT * FROM times WHERE room_id=$_GET['room'];";
-            echo "$_GET['room']";
-            $result = pg_query($sql) or die('Query failed: ' . pg_last_error());
+            //$dbconn = pg_connect("host=db.cs.wm.edu dbname=swyao_CBS user=nswhay password=nswhay")
+            //  or die('Could not connect:' . pg_last_error());
+            //$sql = "SELECT * FROM times WHERE room_id=$_GET['room'];";
+            echo "<tr><td>$_GET['room']</td></tr>";
+            //$result = pg_query($sql) or die('Query failed: ' . pg_last_error());
 
             // while ($line = pg_fetch_array($result, null, PGSQL_NUM)) {
             //   echo "\t<tr>\n";
